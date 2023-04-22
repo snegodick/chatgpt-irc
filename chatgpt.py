48d0b066c86cf6d9e93249c635040d5c3ff646cf
snegodick <ne@ovrnet.ru>
2023-04-22 11:16:59 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -4,26 +4,24 @@ import ssl
 import time
 import openai
 import re
-
-openai.api_key = "apikey" #Insert your secret key between quotes
-
-server = "irc.whatever.com"
+openai.api_key = "" #Insert your secret key between quotes
+server = "irc.oftc.net"
 port = 6697 # 6667 for non SSL
-channel = "#insertchannel"
-botnick = "ChatGPT"
+channel = '#/s/'
+botnick = "Pepa"
+allow_names = ["pepa", "pepe", "Pepe", "пепа", "пепе", "Пепе", "Пепа", "пипа", "Пипа", "Pepa"] #How can I contact the bot?
 password = ""
-
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, port))
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
 irc_ssl = context.wrap_socket(irc, server_hostname=server)
-
 irc_ssl.send(("USER " + botnick + " " + botnick + " " + botnick + " :I am a bot!\r\n").encode())
 irc_ssl.send(("NICK " + botnick + "\r\n").encode())
-
-prompt_base = "Roleplay as a ChatGPT bot. What would you like to do or say in response to the following prompt?"
+irc_ssl.send(("JOIN " + channel + "\r\n").encode())
+prompt_base = "Roleplay as a IRC bot. Answer short as possible. Talk like a virgin in 4chan forum. Swear a lot. In russian language. Do not repeat the text if you are asked to."
+conversation_history = [] #don't touch
 
 while True:
     data = irc_ssl.recv(4096).decode('utf-8')
@@ -37,31 +35,42 @@ while True:
         if match:
             user = match.group(1)
             message = match.group(5)
-            if message.startswith(botnick + ":"):
-                prompt = prompt_base + " " + message.split(botnick + ":")[1]
+            if any(message.startswith(name + "") for name in allow_names):
+            #if message.startswith(botnick + ""):
+                #question = message.split(botnick + "")[1]
+                trigger_name = next(name for name in allow_names if message.startswith(name))
+                question = message.split(trigger_name + "")[1]
+                conversation_history.append({"user": user, "message": question})
+                while len(conversation_history) > 8:
+                    conversation_history.pop(0)
+                context = "".join([f"{item['user']}: {item['message']}\n" for item in conversation_history])
+                prompt = f"{context}GPT-3:"
+
                 try:
                     response = openai.Completion.create(
-                        engine="text-davinci-003", # text-curie-001 is 10x cheaper and dumber
-                        prompt=prompt,
-                        max_tokens=2048, #may have to lower if you use curie
+                        engine="text-davinci-003",  # text-curie-001 is 10x cheaper and dumber
+                        #prompt=prompt,
+                        prompt = f"{context}\n{prompt_base} {question}\nGPT-3:",
+                        max_tokens=2048,  # may have to lower if you use curie
                         n=1,
                         stop=None,
-                        temperature=0.2, # higher = more random
+                        temperature=0.4,  # higher = more random
                     )
                     print(response)
                     if response.choices and response.choices[0].text.strip():
                         text = response.choices[0].text.strip()
+                        conversation_history.append({"user": "GPT-3", "message": text})
                         lines = (line.strip() for line in text.split("\n") if line.strip())
                         for i, line in enumerate(lines):
-                            if i >= 10:
-                                irc_ssl.send(f"PRIVMSG {channel} :\x0308Too many lines! Sorry sosu.\x0F\r\n".encode())
+                            if i >= 15:
+                                irc_ssl.send(f"PRIVMSG {channel} :Бля, слишком много строк.\r\n".encode())
                                 break
-                            prefix = "\x0306[ChatGPT]\x0F: " if i == 0 else ""
-                            chunks = [line[x:x+400] for x in range(0, len(line), 400)]
-                            for chunk in chunks:
-                                irc_ssl.send(f"PRIVMSG {channel} :{prefix}\x0312{chunk}\x0F\r\n".encode())
+                            prefix = " " if i == 0 else ""
+                            max_chars_per_line = 256 
+                            for i in range(0, len(line), max_chars_per_line):
+                                chunk = line[i:i+max_chars_per_line]
+                                irc_ssl.send(f"PRIVMSG {channel} :{chunk}\r\n".encode())
                 except Exception as e:
                     print("Error:", str(e))
-                    irc_ssl.send(f"PRIVMSG {channel} :{prefix}\x0308An error occurred while processing your request. Please try again later.\x0F\r\n".encode())
-                            
+                    irc_ssl.send(f"PRIVMSG {channel} :Ошибка при обработке запроса, идите нахуй.\r\n".encode())
     time.sleep(1)
